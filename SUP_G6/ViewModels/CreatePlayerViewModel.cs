7b82bd618cfb053609853ff2ccbeec955413d61e
Mjurtas <marten.bendroth@gmail.com>
2020-09-08 15:30:58 +0200

    Får create att fungera från ViewModel Skapat IPlayer -interface

--- SUP_G6/ViewModels/CreatePlayerViewModel.cs
+++ SUP_G6/ViewModels/CreatePlayerViewModel.cs
@@ -1,13 +1,39 @@
-﻿using SUP_G6.Models;
+﻿using SUP_G6.Interface;
+using SUP_G6.Models;
+using SUP_G6.Other;
+using SUP_G6.ViewModels.BaseViewModel;
 using SUP_G6.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace SUP_G6.ViewModels
 {
-    public class CreatePlayerViewModel 
+    public class CreatePlayerViewModel : BaseViewModel.BaseViewModel
     {
+        public IPlayer Player { get; set; }
+        public ICommand CreatePlayerCommand { get; set; }
+        public string Name { get; set; }
+
+
+        public CreatePlayerViewModel()
+        {
+            
+            CreatePlayerCommand = new RelayCommand(CreatePlayer);
+        }
         
+
+
+        public void CreatePlayer()
+        {
+            Player player = new Player
+            {
+                Name = this.Name
+            };
+
+            DataBaseLogic.AddPlayer(player);
+        }
+
     }
 }
