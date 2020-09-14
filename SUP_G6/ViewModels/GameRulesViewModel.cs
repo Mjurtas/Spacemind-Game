2ba630b0caf716ba5d445814b18778a0d755d142
Magnus N <mangen81@hotmail.com>
2020-09-14 10:17:38 +0200

    ID 13.1 Skapa Page för spelregler

---
+++ SUP_G6/ViewModels/GameRulesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUP_G6.ViewModels
+{
+    class GameRulesViewModel
+    {
+        public string GameRules { get; set; } = "Blalalalalalalalalalala";
+    }
+}
---
+++ SUP_G6/Views/GameRules.xaml.cs
@@ -0,0 +1,37 @@
+﻿using SUP_G6.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SUP_G6.Views
+{
+    /// <summary>
+    /// Interaction logic for GameRules.xaml
+    /// </summary>
+    public partial class GameRulesPage : Page
+    {
+        GameRulesViewModel viewModel;
+        public GameRulesPage()
+        {
+            InitializeComponent();
+            viewModel = new GameRulesViewModel();
+            DataContext = viewModel;
+        }
+
+        private void ChangePageToViewGameRules()
+        {
+            
+
+        }
+
+    }
+}
