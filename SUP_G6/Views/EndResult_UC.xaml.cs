fd60f30cbf534a2114b4c942e1e547e32b201d58
Jouhanna Meless <jome1802@student.miun.se>
2020-09-16 13:18:46 +0200

    Skapat panels för att visa spelresultat vid spelets slut

---
+++ SUP_G6/Views/EndResult_UC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SUP_G6
+{
+    /// <summary>
+    /// Interaction logic for EndResult_UC.xaml
+    /// </summary>
+    public partial class EndResult_UC : UserControl
+    {
+        public EndResult_UC()
+        {
+            InitializeComponent();
+        }
+    }
+}
