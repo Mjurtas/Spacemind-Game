784c9cda27a560ae55a905a94ef10b0355a6652c
Jouhanna Meless <jome1802@student.miun.se>
2020-09-18 10:22:34 +0200

    GamePlayPage - Skapat en empty button för att tömma gissningsrad

--- SUP_G6/Views/GamePlayPage.xaml.cs
+++ SUP_G6/Views/GamePlayPage.xaml.cs
@@ -257,5 +257,10 @@ namespace SUP_G6.Views
             }
         }
         #endregion
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            currentGuessRow.Children.Clear();
+        }
     }
 }
