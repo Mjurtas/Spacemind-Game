30a5f1b4ac50b5d544883f8710bcf3deedbef94f
Mjurtas <marten.bendroth@gmail.com>
2020-09-25 13:02:08 +0200

    Sökfunktion för namn på Chooseplayer Ny ExitIcon

--- SUP_G6/ViewModels/ChoosePlayerViewModel.cs
+++ SUP_G6/ViewModels/ChoosePlayerViewModel.cs
@@ -9,33 +9,52 @@ using System.Text;
 using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Input;
-
+using System.Collections.ObjectModel;
 
 namespace SUP_G6.ViewModels
 {
-    public class ChoosePlayerViewModel
+    public class ChoosePlayerViewModel : BaseViewModel.BaseViewModel
     {
         #region Properties
-        public List<Player> Players { get; set; } = (List<Player>)DataBaseLogic.GetPlayers();
+        public ObservableCollection<Player> Players { get; set; } = DataBaseLogic.GetPlayers();
         public Player Player { get; set; }
         public Level Level { get; set; }
         public string PlayGameButton { get; set; } = "play game";
         public string BackToStartButton { get; set; } = "back";
+        public string SearchPlayerInput { get; set; } = "";
         #endregion
 
         #region ICommand
         public ICommand StartGameCommand { get; set; }
         public ICommand BackButtonCommand { get; set; }
-
+        public ICommand SearchPlayerCommand { get; set; }
+        public ICommand ClearPlayerSearchCommand { get; set; }
         #endregion
 
         public ChoosePlayerViewModel()
         {         
             StartGameCommand = new RelayCommand(StartGame);
             BackButtonCommand = new RelayCommand(BackToStart);
+            SearchPlayerCommand = new RelayCommand(SearchPlayer);
+            ClearPlayerSearchCommand = new RelayCommand(ClearPlayerSearch);
 
         }
 
+        private void ClearPlayerSearch()
+        {
+            SearchPlayerInput = "";
+            Players  = DataBaseLogic.GetPlayers();
+
+        }
+
+        private void SearchPlayer()
+        {
+            if (SearchPlayerInput != "")
+            {
+                Players = DataBaseLogic.GetPlayersByName(SearchPlayerInput);
+            }
+            
+        }
 
         public void StartGame()
         {
