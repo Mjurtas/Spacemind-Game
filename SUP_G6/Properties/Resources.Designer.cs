e1ae75655c9f9277f244473c27c0b44a416b6715
Jouhanna Meless <jome1802@student.miun.se>
2020-09-30 11:16:27 +0200

    Gjort bindings i xaml i alla pages

--- SUP_G6/Properties/Resources.Designer.cs
+++ SUP_G6/Properties/Resources.Designer.cs
@@ -92,16 +92,6 @@ namespace SUP_G6.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        public static byte[] darth1 {
-            get {
-                object obj = ResourceManager.GetObject("darth1", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
         public static byte[] darthRanked {
             get {
                 object obj = ResourceManager.GetObject("darthRanked", resourceCulture);
